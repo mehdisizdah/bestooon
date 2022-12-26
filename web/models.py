80b4a26bf7836f76048c99f1d6e61155dccaf643
mehdi <msc13.ict@gmail.com>
2022-12-26 20:00:09 +0330

    income in models and beautification

--- web/models.py
+++ web/models.py
@@ -7,3 +7,13 @@ class Expense(models.Model):
     date=models.DateTimeField()
     amount=models.BigIntegerField()
     user=models.ForeignKey(User,on_delete=models.PROTECT)
+    def __str__(self):                                  #__unicode__ kar nakard bar khalafe ammozesh
+        return "{} , {}T".format(self.user,self.amount)
+
+class Income(models.Model):
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User,on_delete=models.RESTRICT)
+    def __str__(self):
+        return "{} , {}T".format(self.user,self.amount)
